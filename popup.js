eb3ddeba419ec667db95a2868210a9e31fe6e558
Anurag Gupta <guptaanurag2106@gmail.com>
2021-04-25 20:27:06 +0530

    add style and input changes

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
 window.onload = function () {
     document.getElementById("save").onclick = function () {
+
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
+        if (username != "" && password != "") {
+            document.getElementById("saved").innerHTML = "Saved!!";
+            setTimeout(() => { document.getElementById("saved").innerHTML = "" }, 1500);
 
-        chrome.storage.sync.set({ "username": username })
-        chrome.storage.sync.set({ "password": password })
 
-        document.getElementById("username").value = "";
-        document.getElementById("password").value = "";
-
+            chrome.storage.sync.set({ "username": username });
+            chrome.storage.sync.set({ "password": password });
+        }
+        else if (username == "" && password != "") {
+            document.getElementById("saved").innerHTML = "Enter Username!!";
+            setTimeout(() => { document.getElementById("saved").innerHTML = "" }, 1500);
+        }
+        else if (password == "" && username != "") {
+            document.getElementById("saved").innerHTML = "Enter Password!!";
+            setTimeout(() => { document.getElementById("saved").innerHTML = "" }, 1500);
+        } else {
+            document.getElementById("saved").innerHTML = "Enter Username and Password!!";
+            setTimeout(() => { document.getElementById("saved").innerHTML = "" }, 1500);
+        }
 
     }
 }
+
